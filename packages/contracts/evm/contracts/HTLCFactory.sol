ccdf5285f4c162189c241c15dead0909c132c552
ftNikhil.eth <nv5475609@gmail.com>
2025-07-28 17:27:32 +0530

    fix: contract versions

--- packages/contracts/evm/contracts/HTLCFactory.sol
+++ packages/contracts/evm/contracts/HTLCFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -41,7 +41,7 @@ contract HTLCFactory is Ownable, ReentrancyGuard {
     uint256 public constant MIN_TIMELOCK = 1 hours;
     uint256 public constant MAX_TIMELOCK = 7 days;
     
-    constructor() {}
+    constructor() Ownable(msg.sender) {}
 
     /**
      * @dev Deploy source HTLC contract
